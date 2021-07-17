0e3b453cd0f0c77ca910f0e481dc8059f7c083f2
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-17 16:56:09 -0700

    refactored

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const router = require("express").Router();
 // To join paths
-const path = require("path");
+// const path = require("path");
 // To use express method for file read and write
 const fs = require("fs");
 // Gets notes data
@@ -25,33 +25,36 @@ router.post("/api/notes", (req, res) => {
   // const newNote = { body, _id: uuidv4() };
   //   notes.push(newNote);
   //   res.json(notes);
-  fs.readFile("./db/db.json", "utf8", (error, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     const notesData = JSON.parse(data);
     body.id = uuidv4();
     notesData.push(body);
     res.send(notesData);
 
-    fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
-      if (err) {
-        console.log(err);
+    fs.writeFile("./db/db.json", JSON.stringify(notesData), error => {
+      if (error) {
+        console.log(error);
       }
     });
+    if (err) {
+      console.log(err);
+    }
   });
 });
 
 router.delete("/api/notes/:id", (req, res) => {
   const { id } = req.params;
-  let notesToKeep;
+  // let notesToKeep;
   fs.readFile("./db/db.json", "utf8", (error, data) => {
     const notesData = JSON.parse(data);
     if (error) {
       console.log(error)
     }
-    notesToKeep = notesData.filter((note) => {
+    let notesToKeep = notesData.filter((note) => {
       return note.id !== id;
     });
     console.log(notesData);
-    fs.writeFile("./db/db.json", JSON.stringify(notesToKeep), (error) => {
+    fs.writeFile("./db/db.json", JSON.stringify(notesToKeep), error => {
       if (error) {
         console.log(error);
       }
