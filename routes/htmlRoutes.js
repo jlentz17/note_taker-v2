12089ca8d425b0a944ceea8d949c2276bda90175
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-07-16 20:48:41 -0700

    added * get request

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 // define variable for path to be used
 const path = require("path");
 // get data and send to index.html
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirName, "../public/index.html"));
+});
+
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirName, "../public/index.html"));
 });
